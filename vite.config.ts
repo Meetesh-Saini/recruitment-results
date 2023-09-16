d816ede36db8f5cc0f5d89cfa8b9b6adf98af039
Meetesh Saini <code.meeteshsaini@gmail.com>
2023-09-16 22:43:48 +0530

    added base in config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
 export default defineConfig({
+  base: "/recruitment-results/",
   plugins: [react(),],
   css: {
     postcss: {
